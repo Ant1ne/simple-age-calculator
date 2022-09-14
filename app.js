13c88fddf862ee7effe67ee0d08b93831bbb1f8c
Antoine <110994502+Ant1ne@users.noreply.github.com>
2022-09-14 14:11:49 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var name = prompt('What is your name?');
+alert('Hi ' + name)
+
+var dob = prompt('What year were you born?');
+var yearTill = prompt('What year till you want to calculte your age?');
+
+alert(yearTill - dob);
